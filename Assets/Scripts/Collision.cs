23bf0b6cc08933b0896efe1558324fdb4987b454
Dan Timmins <dantimmins97@gmail.com>
2024-03-20 16:52:07 -0400

    Basic OnTriggerEnter test

--- Assets/Scripts/Collision.cs
+++ Assets/Scripts/Collision.cs
@@ -8,4 +8,9 @@ public class Collision : MonoBehaviour
     {
         Debug.Log("Drive better noob!");
     }
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        Debug.Log("O...M...G! Triggered!");    
+    }
 }
