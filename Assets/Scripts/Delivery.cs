70e7b2ad683af756b5119c710649c602f14942b3
Dan Timmins <dantimmins97@gmail.com>
2024-03-22 18:14:55 -0400

    Basic package delivery

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Collision : MonoBehaviour
+public class Delivery : MonoBehaviour
 {
     void OnCollisionEnter2D(Collision2D other) 
     {
@@ -11,6 +11,13 @@ public class Collision : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other) 
     {
-        Debug.Log("O...M...G! Triggered!");    
+        if(other.tag == "Package")
+        {
+            Debug.Log("Package picked up!");
+        } 
+        else if(other.tag == "Customer")
+        {
+            Debug.Log("Package dropped off!");
+        }  
     }
 }
