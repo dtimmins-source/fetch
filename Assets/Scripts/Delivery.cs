e136c3539bcfb21bb38d65a74dbe5cc1694aa59d
Dan Timmins <dantimmins97@gmail.com>
2024-03-22 18:23:03 -0400

    Conditional package delivery

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    bool hasPackage;
+
     void OnCollisionEnter2D(Collision2D other) 
     {
         Debug.Log("Drive better noob!");
@@ -11,13 +13,15 @@ public class Delivery : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other) 
     {
-        if(other.tag == "Package")
+        if(other.tag == "Package" && hasPackage == false)
         {
             Debug.Log("Package picked up!");
+            hasPackage = true;
         } 
-        else if(other.tag == "Customer")
+        else if(other.tag == "Customer" && hasPackage)
         {
             Debug.Log("Package dropped off!");
+            hasPackage = false;
         }  
     }
 }
